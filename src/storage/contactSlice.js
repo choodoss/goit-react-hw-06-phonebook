b3ded8875c74f3ca8ba325fc84fa3ef69c732bed
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-06-13 19:57:42 +0300

    fix

--- src/storage/contactSlice.js
+++ src/storage/contactSlice.js
@@ -1,36 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    contacts: [{
-        id: "1",
-        name: 'Андрій Коваль',
-        phone: '+380501234567'
-    },
-    {
-        id: "2",
-        name: 'Софія Шевченко',
-        phone: '+380631234567'
-    },
-    {
-        id: "4",
-        name: 'Юлія Мартинюк',
-        phone: '+380991234567'
-    },
-    {
-        id: "5",
-        name: 'Василь Іванов',
-        phone: '+380501112233'
-    }]
+    contacts: []
 }
 const contactSlice = createSlice({
     name: 'contacts',
     initialState,
     reducers: {
         addContact: (state, { payload }) => {
-            state.contacts = [...state.contacts, payload].sort((a, b) => a.name.localeCompare(b.name));
+            return [...state.contacts, payload].sort((a, b) => a.name.localeCompare(b.name));
         },
         removeContact: (state, { payload }) => {
-            state.contacts = state.contacts.filter(contact => !contact.id.includes(payload));
+            return state.contacts.filter(contact => !contact.id.includes(payload));
         }
     }
 });
